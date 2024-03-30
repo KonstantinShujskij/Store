35640e4611f9ed617f6586efc6448138eaa3dcb4
Shuyskiy Konstantin <shujskij.konstantin@gmail.com>
2024-03-30 17:13:18 +0100

    Initial

---
+++ app.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose')
+const express = require('express')
+const cors = require('cors')
+
+const config = require('config')
+const path = require('path')
+
+
+const app = express()
+
+app.use(cors())
+app.use(express.json({ extended: true }))
+
+app.use('/store', express.static(path.join(__dirname, 'store')))
+
+
+const PORT = config.get('port')
+const SLL_PORT = config.get('sslPort')
+
+
+async function start() {
+    try {
+        const mongoUri = config.get('mongoUri')
+        await mongoose.connect(mongoUri, { useNewUrlParser: true, useUnifiedTopology: true })
+
+        app.listen(PORT, () => console.log(`App has been started on port ${PORT}`))
+    } catch(error) {
+        console.log("Server error: ", error.message)
+        process.exit(1)
+    }
+}
+
+start()
